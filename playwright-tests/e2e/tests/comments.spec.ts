f4b034a7423c4b395561f326ccf6cca95c5ca75f
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2024-02-13 23:40:12 +0530

    fix: removed  redundant awaits

--- playwright-tests/e2e/tests/comments.spec.ts
+++ playwright-tests/e2e/tests/comments.spec.ts
@@ -1,5 +1,5 @@
 //comments-spec.ts
-import { expect } from "@playwright/test";
+import { Browser, Page, expect } from "@playwright/test";
 import { test } from "../fixtures/index";
 import { Users } from "../../constants";
 import { TaskPage } from "../pom/tasks";
@@ -26,12 +26,11 @@ test.describe("Comment Page", () => {
   }) => {
     page.goto("/");
 
-    await test.step("step 1 - create a new task by assigner", async () => {
-      await taskPage.createTaskAndVerify({
+    await test.step("step 1 - create a new task by assigner", () =>
+      taskPage.createTaskAndVerify({
         taskName: todoName,
         userName: "Sam Smith",
-      });
-    });
+      }));
 
     await test.step("step 2 - go to that newely created task", async () => {
       await page.getByTestId("tasks-pending-table").getByText(todoName).click();
@@ -42,27 +41,32 @@ test.describe("Comment Page", () => {
       expect(await page.locator("h1")).toHaveText(todoName);
     });
 
-    await test.step("step 3 - create comment as assigner in the todo and verify", async () => {
-      await taskPage.createCommentAndVerify({
+    await test.step("step 3 - create comment as assigner in the todo and verify", () =>
+      taskPage.createCommentAndVerify({
         comment: assignerComment,
         taskName: todoName,
-      });
-    });
+      }));
 
-    await test.step("step 4 - check if comment count was increased for the task", async () => {
-      await taskPage.checkForCount({
+    await test.step("step 4 - check if comment count was increased for the task", () =>
+      taskPage.checkForCount({
         taskName: todoName,
         count: 1,
+      }));
+
+    let assigneeContext: Browser;
+    let assigneePage: Page;
+    let assigneeTasks: TaskPage;
+    let assigneeAuth: LoginPage;
+
+    await test.step("step 5 - create new browser window", async () => {
+      assigneeContext = await browser.newContext({
+        storageState: { cookies: [], origins: [] },
       });
-    });
 
-    const assigneeContext = await browser.newContext({
-      storageState: { cookies: [], origins: [] },
+      assigneePage = await assigneeContext.newPage();
+      assigneeAuth = new LoginPage(assigneePage);
+      assigneeTasks = new TaskPage(assigneePage);
     });
-
-    const assigneePage = await assigneeContext.newPage();
-    const assigneeAuth = new LoginPage(assigneePage);
-    const assigneeTasks = new TaskPage(assigneePage);
 
     await test.step("step 5  - Login as assigned user and go to the specified task", async () => {
       await assigneePage.goto("/");
@@ -80,19 +84,17 @@ test.describe("Comment Page", () => {
       expect(await assigneePage.locator("h1").innerText()).toBe(todoName);
     });
 
-    await test.step("step 6 - Create comment as assigned user", async () => {
-      await assigneeTasks.createCommentAndVerify({
+    await test.step("step 6 - Create comment as assigned user", () =>
+      assigneeTasks.createCommentAndVerify({
         comment: assigneeComment,
         taskName: todoName,
-      });
-    });
+      }));
 
-    await test.step("step 6 - check for comment count", async () => {
-      await assigneeTasks.checkForCount({
+    await test.step("step 6 - check for comment count", () =>
+      assigneeTasks.checkForCount({
         taskName: todoName,
         count: 2,
-      });
-    });
+      }));
 
     assigneePage.close();
     assigneeContext.close();
